be4df2ab8545f5dfacfee8f2640a6fdf45a3631e
Andrew Gomes <andrewgomes1328@gmail.com>
2023-05-24 20:26:57 -0300

    update

---
+++ js/botoes-sobre.js
@@ -0,0 +1,45 @@
+const btnsobre = document.getElementById('sobre');
+const btnform = document.getElementById('form');
+const btnobj = document.getElementById('obj');
+
+const card = document.getElementById("card-um");
+const card2 = document.getElementById("card-dois");
+const card3 = document.getElementById("card-tres");
+
+btnsobre.addEventListener('click', () => {
+  card.style.display = 'block';
+  btnsobre.style.background = 'green';
+  btnform.style.background = '#73beff';
+  btnobj.style.background = '#73beff';
+  card2.style.display = 'none';
+  card3.style.display = 'none';
+});
+
+btnform.addEventListener('click', () => {
+  card2.style.display = 'block';
+  btnform.style.background = 'green';
+  btnsobre.style.background = '#73beff';
+  btnobj.style.background = '#73beff';
+  card.style.display = 'none';
+  card3.style.display = 'none';
+});
+
+btnobj.addEventListener('click', () => {
+  card3.style.display = 'block';
+  btnobj.style.background = 'green';
+  btnform.style.background = '#73beff';
+  btnsobre.style.background = '#73beff';
+  card.style.display = 'none';
+  card2.style.display = 'none';
+});
+
+var botaoModoEscuro = document.getElementById("alternar-cor");
+var body = document.body;
+var art1 = document.getElementById("art1-sobre");
+var habilidades = document.getElementById("habilidades");
+
+botaoModoEscuro.addEventListener("click", function() {
+  body.classList.toggle("dark-mode");
+  art1.classList.toggle('art11');
+  habilidades.classList.toggle('art12');
+});
